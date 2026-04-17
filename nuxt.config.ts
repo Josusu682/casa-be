46b405671d2b177ff3bf6cf8bae8c6d4633b8e34
Josué Román <josue.nrj1@gmail.com>
2026-04-17 19:24:48 -0400

    nuxt config 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
   modules: ['@nuxtjs/tailwindcss'],
-
   css: ['~/assets/styles/main.css'],
-
+  
   app: {
+    // ⚠️ REEMPLAZA 'Casa-BE-Josu' por el nombre exacto de tu repo en GitHub
+    baseURL: '/casa-be/', 
+    buildAssetsDir: 'assets',
     head: {
-      title: 'casa-be',
+      title: 'Casa BE',
+
+
+
       meta: [
         { name: 'description', content: 'Un espacio presencial donde la neurociencia y el bienestar se encuentran.' }
       ],
