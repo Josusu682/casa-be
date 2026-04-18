8e333277bf159ababb1c58d7f3f33a5da082e045
Josué Román <josue.nrj1@gmail.com>
2026-04-17 20:53:09 -0400

    fix: configurar nitro preset y ssr para github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,26 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
+  // 1. Apagamos el Server Side Rendering
+  ssr: false, 
+  
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/styles/main.css'],
-  
+
   app: {
-    // ⚠️ REEMPLAZA 'Casa-BE-Josu' por el nombre exacto de tu repo en GitHub
+    // 2. Rutas correctas
     baseURL: '/casa-be/', 
-    buildAssetsDir: 'assets',
+    buildAssetsDir: 'assets', 
     head: {
       title: 'Casa BE',
-
-
-
       meta: [
         { name: 'description', content: 'Un espacio presencial donde la neurociencia y el bienestar se encuentran.' }
       ],
     }
+  },
+
+  // 3. EL TRUCO MÁGICO: Le decimos al motor cómo compilar para GitHub
+  nitro: {
+    preset: 'github-pages'
   }
 })
